02171329ff1f452780620ba1d0e8e67d9aeaa2ea
Aria Taylor <ari@aricodes.net>
2023-03-20 15:45:12 -0400

    Load dotenv file

--- spec/whisprite_spec.rb
+++ spec/whisprite_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Whisprite do
   it "has a version number" do
     expect(Whisprite::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
