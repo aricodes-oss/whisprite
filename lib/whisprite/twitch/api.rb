842f909dc92862f97be77d1a3c625992c531ee46
Aria Taylor <ari@aricodes.net>
2023-03-26 16:49:28 -0400

    Naming thingy

--- lib/whisprite/twitch/api.rb
+++ lib/whisprite/twitch/api.rb
@@ -4,9 +4,9 @@ require "twitchrb"
 
 module Whisprite::Twitch
   def self.api
-    return @api_client unless @api_client.nil?
+    return @chat_client unless @chat_client.nil?
 
-    @api_client = Twitch::Client.new(
+    @chat_client = Twitch::Client.new(
       client_id: ENV.fetch("TWITCH_CLIENT_ID"),
       access_token: ENV.fetch("TWITCH_ACCESS_TOKEN")
     )
