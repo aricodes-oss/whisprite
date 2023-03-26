2c285aaecffe9657cc225294365f64525527ee99
Aria Taylor <ari@aricodes.net>
2023-03-26 18:51:42 -0400

    Consolidate

--- lib/whisprite.rb
+++ lib/whisprite.rb
@@ -3,18 +3,89 @@
 require "dotenv/load"
 require "yaml"
 require "active_record"
+require "twitch/bot"
 
-# Load and initialize ActiveRecord
+module Whisprite
+  class Error < StandardError; end
+end
+
+# Load and initialize ActiveRecord specifically
 project_root = File.dirname(File.absolute_path(__FILE__))
 Dir.glob("#{project_root}/models/*.rb").sort.each { |f| require f }
 
 connection_details = YAML.safe_load(File.open("config/database.yml"))
 ActiveRecord::Base.establish_connection(connection_details)
 
-module Whisprite
-  class Error < StandardError; end
-end
-
-# Load sub modules
+# ...then load sub modules
 require "require_all"
 require_all "#{File.dirname(__FILE__)}/whisprite"
+
+module Whisprite
+  class Bot
+    def initialize
+      @client = Twitch::Bot::Client.new(
+        channel: ENV.fetch("TWITCH_CHANNEL", nil),
+        config: configuration
+      ) do
+        # register_handler(Whisprite::JoinHandler)
+        # register_handler(Whisprite::SubscriptionHandler)
+        # register_handler(Whisprite::PlanCommandHandler)
+        # register_handler(Whisprite::StreamerCommandHandler)
+        # register_handler(Whisprite::QuoteCommandHandler)
+        # register_handler(Whisprite::DefendCommandHandler)
+        # register_handler(Whisprite::AttackCommandHandler)
+        # register_handler(Whisprite::ShoutoutHandler)
+      end
+    end
+
+    def run
+      client.run
+    end
+
+    private
+
+    attr_reader :client
+
+    def adapter_class
+      if development_mode?
+        "Twitch::Bot::Adapter::Terminal"
+      else
+        "Twitch::Bot::Adapter::Irc"
+      end
+    end
+
+    def configuration
+      Twitch::Bot::Config.new(
+        settings: {
+          botname: ENV.fetch("TWITCH_USERNAME", nil),
+          irc: {
+            nickname: ENV.fetch("TWITCH_USERNAME", nil),
+            password: ENV.fetch("TWITCH_PASSWORD", nil)
+          },
+          adapter: adapter_class,
+          memory: "Twitch::Bot::Memory::Redis",
+          log: {
+            file: logfile,
+            level: loglevel
+          }
+        }
+      )
+    end
+
+    def logfile
+      if ENV["BOT_LOGFILE"]
+        File.new(ENV["BOT_LOGFILE"], "w")
+      else
+        $stdout
+      end
+    end
+
+    def loglevel
+      (ENV["BOT_LOGLEVEL"] || "info").to_sym
+    end
+
+    def development_mode?
+      ENV["BOT_MODE"] == "development"
+    end
+  end
+end
--- lib/whisprite/twitch.rb
+++ lib/whisprite/twitch.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "twitch/api"
+require "twitchrb"
 
-module Whisprite::Twitch
+module Whisprite
+  def self.api
+    return @chat_client unless @chat_client.nil?
+
+    @chat_client = Twitch::Client.new(
+      client_id: ENV.fetch("TWITCH_CLIENT_ID"),
+      access_token: ENV.fetch("TWITCH_ACCESS_TOKEN")
+    )
+  end
 end
